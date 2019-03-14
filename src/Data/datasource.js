37bba92ed52ba204222ff68f47950ff21859a649
sanddhy <sandy.mulato.r@gmail.com>
2019-03-14 11:22:20 -0600

    Avatar

--- src/Data/datasource.js
+++ src/Data/datasource.js
@@ -26,5 +26,45 @@ export const jobsList = [
       start: 'Nov',
       end: '2018'
     }
+  },
+  {
+    title: 'Teleoperador',
+    company: 'Atento.',
+    description: `Consumer and automotive credits analyst, Information validation, Responsible for the approval of consumer and automotive credits.`,
+    years: {
+      start: '2017',
+      end: '2018'
+    }
+  }
+]
+
+export const data = [
+  {
+    name: 'Nodejs',
+    porcentaje: 80
+  },
+  {
+    name: 'JavaScript',
+    porcentaje: 80
+  },
+  {
+    name: 'css',
+    porcentaje: 50
+  },
+  {
+    name: 'Html',
+    porcentaje: 50
+  },
+  {
+    name: 'React',
+    porcentaje: 60
+  },
+  {
+    name: 'Material',
+    porcentaje: 70
+  },
+  {
+    name: 'Mongoose',
+    porcentaje: 60
   }
 ]
